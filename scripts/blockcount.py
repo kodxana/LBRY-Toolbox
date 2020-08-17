c48f89d4a31b6bfc7d1caf578cfd2f28a54be27b
kodxana <romanrex19@int.pl>
2020-08-17 23:36:40 +0200

    Fixed typo

--- scripts/blockcount.py
+++ scripts/blockcount.py
@@ -1,3 +1,5 @@
-rom bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+# https://github.com/jgarzik/python-bitcoinrpc
+
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 rpc_connection = AuthServiceProxy("http://rpcuser:rpcpass@127.0.0.1:9245", timeout=120)
 print(rpc_connection.getblockcount())
